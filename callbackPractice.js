615693cc139077aefa0d2754053ec05721ad77de
Bryan Schauerte <bryan.schauerte@gmail.com>
2015-09-02 11:59:12 -0600

    completed exercises concerning callbacks

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,16 +1,16 @@
 /* In this repo your job is to write functions to make each function call work properly.
-Below is a sample problem 
+Below is a sample problem
 
   //code here for sayHi
 
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay);
    });
-   
 
-and what you should write is the favNum function that makes the code above work, 
-    
-    
+
+and what you should write is the favNum function that makes the code above work,
+
+
    var sayHi = function(str, cb){
     cb(str);
    }
@@ -18,19 +18,23 @@ and what you should write is the favNum function that makes the code above work,
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay); //should alert ('Hi Katie')'
    });
-    
-    
+
+
 */
 
 
 
   //Code Here for first
-  
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var first = function(array, callBack){
+  callBack(array[0]);
+};
+
 first(names, function(firstName){
   console.log('The first name in names is ', firstName)
 });
-
 
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
@@ -40,7 +44,14 @@ first(names, function(firstName){
 
   //Code Here for last
 
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+ var last = function(array, callBack){
+  callBack(array[array.length-1]);
+};
+
+
 last(names, function(lastName){
   console.log('The last name in names is ', lastName);
 });
@@ -58,6 +69,10 @@ last(names, function(lastName){
 
   //Code Here for multiply
 
+  var multiply = function(a,b, callBack){
+  return  callBack(a * b);
+  };
+
 multiply(4, 3, function(answer){
   console.log('The answer is ', answer); //should console.log 12
 })
@@ -71,10 +86,17 @@ multiply(4, 3, function(answer){
 
 
 
+return array.map(function(item){
 
   //Code Here for contains
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var contains = function(array, name, callBack) {
+   array.map(callBack(name));
+
+};
+
 contains(names, 'Colt', function(result){
   if(result === true){
     console.log('Colt is in the array');
@@ -95,6 +117,28 @@ contains(names, 'Colt', function(result){
     //Code Here for uniq
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var uniq = function(array, callBack){
+
+  var uniqArr = [];
+
+  for(var i = 0; i < array.length; i++){
+
+    var test = array[i];
+    var okay = true;
+    for(var p = 0; p < uniqArr.length; p++){
+
+      if(test == uniqArr[p]){
+        okay = false;
+      }
+    }
+
+    if(okay) {
+      uniqArr.push(test);}
+  }
+  return callBack(uniqArr);
+}
+
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
@@ -111,11 +155,37 @@ uniq(names, function(uniqArr){
     //Code Here for each
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var each = function(array, callBack){
+
+for(var i =0; i < array.length; i++){
+  var item = array[i];
+  var indice = i;
+  callBack(item, indice);
+}
+
+};
+
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
+//Extra -- Same output, diff function
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var each = function(array, callBack) {
+  array.map(function(item, index){
+    return callBack(item, index)} );
+};
+
+
+
+each(names, function(item, indice){
+  console.log('The item in the ' + indice + ' position is ' + item)
+});
+
+//End of extra
 
 
 
@@ -148,6 +218,16 @@ var users = [
   },
 ];
 
+var getUserById= function(users, name, callBack){
+
+  for(var i = 0; i < users.length; i++){
+
+    if(users[i]['id'] == name){
+      return callBack(users[i])
+    }
+  }
+
+}
 getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
 });
